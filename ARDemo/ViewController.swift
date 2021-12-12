55706070998f7c6f8eed44d47b8f985581cb4c7b
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2021-12-12 21:43:40 +0300

    Adding a sphere with a human face texture

--- ARDemo/ViewController.swift
+++ ARDemo/ViewController.swift
@@ -20,27 +20,38 @@ class ViewController: UIViewController {
 
         let scene = SCNScene()
         
-        let boxGeometry = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0.05)
-        let material = SCNMaterial()
-        let materialTwo = SCNMaterial()
-        let materialThree = SCNMaterial()
-        material.diffuse.contents = UIColor.purple
-        materialTwo.diffuse.contents = UIColor.yellow
-        materialThree.diffuse.contents = UIColor.green
-        let boxNode = SCNNode(geometry: boxGeometry)
-        boxNode.geometry?.materials = [material, materialTwo, materialThree]
-        boxNode.position = SCNVector3(0, 0, -1.0)
-        scene.rootNode.addChildNode(boxNode)
+        let sphereGepmetry = SCNSphere(radius: 0.1)
+        let sphereMaterial = SCNMaterial()
         
-        let textGeometry = SCNText(string: "Hello, World!", extrusionDepth: 2.0)
-        let textMaterial = SCNMaterial()
-        textMaterial.diffuse.contents = UIColor.red
-//        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
-        let textNode = SCNNode(geometry: textGeometry)
-        textNode.scale = SCNVector3(0.005, 0.005, 0.005)
-        textNode.geometry?.materials = [textMaterial]
-        textNode.position = SCNVector3(0, 0.2, -1.0)
-        scene.rootNode.addChildNode(textNode)
+        
+        sphereMaterial.diffuse.contents = UIImage(named: "head")
+        let sphereNode = SCNNode(geometry: sphereGepmetry)
+        sphereNode.geometry?.materials = [sphereMaterial]
+        sphereNode.position = SCNVector3(0, 0, -1)
+        scene.rootNode.addChildNode(sphereNode)
+        
+        
+//        let boxGeometry = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0.05)
+//        let material = SCNMaterial()
+//        let materialTwo = SCNMaterial()
+//        let materialThree = SCNMaterial()
+//        material.diffuse.contents = UIColor.purple
+//        materialTwo.diffuse.contents = UIColor.yellow
+//        materialThree.diffuse.contents = UIColor.green
+//        let boxNode = SCNNode(geometry: boxGeometry)
+//        boxNode.geometry?.materials = [material, materialTwo, materialThree]
+//        boxNode.position = SCNVector3(0, 0, -1.0)
+//        scene.rootNode.addChildNode(boxNode)
+//
+//        let textGeometry = SCNText(string: "Hello, World!", extrusionDepth: 2.0)
+//        let textMaterial = SCNMaterial()
+//        textMaterial.diffuse.contents = UIColor.red
+////        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+//        let textNode = SCNNode(geometry: textGeometry)
+//        textNode.scale = SCNVector3(0.005, 0.005, 0.005)
+//        textNode.geometry?.materials = [textMaterial]
+//        textNode.position = SCNVector3(0, 0.2, -1.0)
+//        scene.rootNode.addChildNode(textNode)
         
         sceneView.scene = scene
     }
