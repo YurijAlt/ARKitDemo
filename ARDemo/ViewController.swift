977f8e5ca0d31b4b9daf119fa3f59846076c8173
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2021-12-13 23:15:06 +0300

    Creating the Plane class

--- ARDemo/ViewController.swift
+++ ARDemo/ViewController.swift
@@ -9,37 +9,20 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    
     @IBOutlet var sceneView: ARSCNView!
-    var session: ARSession {
-        return sceneView.session
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sceneView.delegate = self
+        
         sceneView.showsStatistics = true
-        sceneView.autoenablesDefaultLighting = true
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
 
         let scene = SCNScene()
         sceneView.scene = scene
-    }
-    
-    
-    private func createBox(in scene: SCNScene) {
-        let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
-        let boxMaterial = SCNMaterial()
-        
-        boxMaterial.diffuse.contents = UIColor.red
-        boxMaterial.specular.contents = UIColor.yellow
-        
-        let boxNode = SCNNode(geometry: box)
-        boxNode.name = "box"
-        boxNode.geometry?.materials = [boxMaterial]
-        boxNode.position = SCNVector3(0.0, 0.0, -0.5)
-        scene.rootNode.addChildNode(boxNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -57,20 +40,6 @@ class ViewController: UIViewController {
         
         // Pause the view's session
         sceneView.session.pause()
-    }
-    
-    @IBAction func resetTapped(sender: UIButton) {
-        session.pause()
-        sceneView.scene.rootNode.enumerateChildNodes { node, _ in
-            if node.name == "box" {
-                node.removeFromParentNode()
-            }
-        }
-        let configuration = ARWorldTrackingConfiguration()
-        session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
-    }
-    @IBAction func addTapped(sender: UIButton) {
-        createBox(in: sceneView.scene)
     }
 
 }
