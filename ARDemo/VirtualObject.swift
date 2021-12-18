e2bbc18f61f4f5465cca6f762ec566beffb8f2e1
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2021-12-18 23:21:06 +0300

    Adding a Virtual Object

---
+++ ARDemo/VirtualObject.swift
@@ -0,0 +1,22 @@
+//
+//  VirtualObject.swift
+//  ARDemo
+//
+//  Created by Юрий Альт on 18.12.2021.
+//
+
+import SceneKit
+
+class VirtualObject: SCNReferenceNode {
+    static let availableObjects: [SCNReferenceNode] = {
+        guard let modelsURLs = Bundle.main.url(forResource: "ARDemo", withExtension: nil) else { return [] }
+        
+        let fileEnumerator = FileManager().enumerator(at: modelsURLs, includingPropertiesForKeys: nil)!
+        
+        return fileEnumerator.flatMap { element in
+            let url = element as! URL
+            guard url.pathExtension == "scn" else { return nil }
+            return VirtualObject(url: url)
+        }
+    }()
+}
